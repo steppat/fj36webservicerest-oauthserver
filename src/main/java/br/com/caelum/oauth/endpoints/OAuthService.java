2f2d972c268af5c0d9b0c0c7c2573f7e71e301a9
soeirosantos <soeirosantos@gmail.com>
2013-12-16 16:22:36 -0200

    init

---
+++ src/main/java/br/com/caelum/oauth/endpoints/OAuthService.java
@@ -0,0 +1,7 @@
+package br.com.caelum.oauth.endpoints;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/oauth")
+public class OAuthService extends Application { }
