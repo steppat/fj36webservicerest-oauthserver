a6014babef9110f8be58d6ac1ea74c00a49bec6d
Nico Steppat <steppat@gmail.com>
2019-06-27 12:46:38 -0300

    adicionando context path no application.properties

---
+++ src/test/java/br/com/caelum/oauth/endpoints/TestTockenEndpoint.java
@@ -0,0 +1,30 @@
+package br.com.caelum.oauth.endpoints;
+
+import org.apache.oltu.oauth2.client.OAuthClient;
+import org.apache.oltu.oauth2.client.URLConnectionClient;
+import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
+import org.apache.oltu.oauth2.client.response.OAuthAccessTokenResponse;
+import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
+import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
+import org.apache.oltu.oauth2.common.message.types.GrantType;
+import org.junit.Test;
+
+public class TestTockenEndpoint {
+
+//	@Test
+	public void testGeraTokenFormHeroku() throws OAuthSystemException, OAuthProblemException {
+
+		String tokenEndpoint = "http://fj36webservicerest-oauthserver.herokuapp.com/oauth/token";
+		
+		OAuthClientRequest request = OAuthClientRequest.tokenLocation(tokenEndpoint).setGrantType(GrantType.PASSWORD)
+				.setClientId("oauth2_client_id").setClientSecret("oauth2_client_secret").setUsername("fake_user")
+				.setPassword("passwd").buildBodyMessage();
+		
+		OAuthClient oAuthClient = new OAuthClient(new URLConnectionClient());
+		OAuthAccessTokenResponse oauthResponse = oAuthClient.accessToken(request);
+		
+		System.out.println("Access token: " + oauthResponse.getAccessToken());
+		System.out.println("Expira em: " + oauthResponse.getExpiresIn());
+	}
+
+}
